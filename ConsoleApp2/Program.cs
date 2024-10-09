f44ebceb250a533b8384fda1fd2d5ad58e52f7e2
2021121 <2021121@B104-14>
2024-10-09 08:54:58 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A kor osztály felhasználása
+            #region 1.objektum
+            //Első objektum
+            kor k1 = new kor(); // Objektum példány létrehozása üresen
+            k1.ReadSugar(15.0); // Sugár megadása
+            k1.SetKerulet();
+            k1.SetTerulet();
+            Console.WriteLine($"A {k1.GetSugar()} sugarú kör kerülete{k1.GetKerulet()}, területe{k1.GetTerulet()}");
+            #endregion EOF 1.objektum
+
+            #region 2.objektumunk
+            kor k2 = new kor(28.7);
+            k2.SetKerulet();
+            k2.SetTerulet();
+            Console.WriteLine($"A {k2.GetSugar()} sugarú kör kerülete{k2.GetKerulet()}, területe{k2.GetTerulet()}");
+            #endregion EOF 2.objektum
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp2/kor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class kor
+    {
+        // Az osztály feladata: a sugárból kiszámítja a kör területét, kerületét//
+        //konstruktorok//
+        //osztályváltozók//
+        private double sugar,
+            kerulet,
+            terulet;
+        public kor(){}
+
+        public kor(double r)
+        {
+            this.sugar = r;
+        }
+
+        public void ReadSugar(double r)
+        {
+            this.sugar = r;
+        }
+
+        public void SetKerulet()
+        {
+            this.kerulet = 2 * this.sugar * Math.PI;
+        }
+
+        public void SetTerulet()
+        {
+            double eredmeny = 0.0,
+                eredmeny2 = 0.0;
+            eredmeny = this.sugar * this.sugar * Math.PI;
+
+            eredmeny2 = Math.Pow(this.sugar, 2)*Math.PI;
+
+            if (eredmeny == eredmeny2)
+            {
+                this.terulet = eredmeny;
+            }
+            else
+            {
+                Console.WriteLine($"Eredmény1 ({eredmeny})= Eredmény2({eredmeny2}");
+            }
+        }
+        public double GetKerulet () { return this.kerulet; }
+        public double GetTerulet() { return this.terulet; }
+        public double GetSugar() { return this.sugar; }
+
+
+    }
+}
